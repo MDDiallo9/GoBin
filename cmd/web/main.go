0b7cae1066e9cd33324db5c89403646115249255
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-06-24 15:29:26 +0200

    Refactoring , now up to Chap 4

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,21 +1,41 @@
 package main
 
 import (
+	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
+type application struct {
+	errorLog *log.Logger
+	infoLog *log.Logger
+}
+
 func main() {
-	mux := http.NewServeMux()
+	port := flag.String("port",":8000","HTTP Network Address") 
 
-	fileServer := http.FileServer(http.Dir("./ui/static/"))
-	mux.Handle("/static/", http.StripPrefix("/static", fileServer))
+	flag.Parse() // Parsing args
 
-	mux.HandleFunc("/{$}", home)
-	mux.HandleFunc("/snippet/view", snippetView)
-	mux.HandleFunc("/snippet/create", snippetCreate)
+	/* Custom Loggers
+	Writing to stdout allows to write logs to a file ( go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.lo) */
+	infoLog := log.New(os.Stdout,"INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stdout,"ERROR\t", log.Ldate|log.Ltime)
 
-	log.Println("Starting server on http://localhost:8000")
-	err := http.ListenAndServe(":8000", mux)
-	log.Fatal(err)
+	// Instanciating a new application struct
+	app := &application{
+		errorLog: errorLog,
+		infoLog: infoLog,
+	}
+
+	/* Initializing a http.Server struct to pass down options */
+	srv := &http.Server{
+		Addr: *port,
+		ErrorLog: errorLog,
+		Handler: app.routes(),
+	}
+
+	infoLog.Println("Starting server on http://localhost:8000")
+	err := srv.ListenAndServe()
+	errorLog.Fatal(err)
 }
