148d52a97f0895353621f50d3f0b4fd788c247aa
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-06-24 15:59:34 +0200

    Added connection to database , up to chapter 4.5

--- cmd/web/main.go
+++ cmd/web/main.go
@@ -1,10 +1,14 @@
 package main
 
 import (
+	"database/sql"
 	"flag"
 	"log"
 	"net/http"
 	"os"
+
+	 _ "github.com/go-sql-driver/mysql"
+	 "github.com/joho/godotenv"
 )
 
 type application struct {
@@ -13,7 +17,13 @@ type application struct {
 }
 
 func main() {
+	// Importing dotenv
+	err := godotenv.Load()
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
 	port := flag.String("port",":8000","HTTP Network Address") 
+	dsn := flag.String("dsn",os.Getenv("MYSQL_DSN"),"MySQL data source name")
 
 	flag.Parse() // Parsing args
 
@@ -21,6 +31,14 @@ func main() {
 	Writing to stdout allows to write logs to a file ( go run ./cmd/web >>/tmp/info.log 2>>/tmp/error.lo) */
 	infoLog := log.New(os.Stdout,"INFO\t", log.Ldate|log.Ltime)
 	errorLog := log.New(os.Stdout,"ERROR\t", log.Ldate|log.Ltime)
+
+	// Database connection
+	db,err := openDB(*dsn)
+	if err != nil {
+		errorLog.Fatal(err)
+	}
+
+	defer db.Close()
 
 	// Instanciating a new application struct
 	app := &application{
@@ -36,6 +54,17 @@ func main() {
 	}
 
 	infoLog.Println("Starting server on http://localhost:8000")
-	err := srv.ListenAndServe()
+	err = srv.ListenAndServe()
 	errorLog.Fatal(err)
 }
+
+func openDB(dsn string) (*sql.DB,error) {
+	db,err := sql.Open("mysql",dsn)
+	if err != nil {
+		return nil,err
+	}
+	if err = db.Ping();err != nil {
+		return nil,err
+	}
+	return db,nil
+} 
