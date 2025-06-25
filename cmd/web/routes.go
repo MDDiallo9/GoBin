f1d7f97645f278bd4f7e385b65a798006d599f6e
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-06-25 14:31:57 +0200

    Added package alice to handle middleware chaining

--- cmd/web/routes.go
+++ cmd/web/routes.go
@@ -1,6 +1,10 @@
 package main
 
-import "net/http"
+import (
+	"net/http"
+
+	"github.com/justinas/alice"
+)
 
 func (app *application) routes() http.Handler {
 	mux := http.NewServeMux()
@@ -12,5 +16,9 @@ func (app *application) routes() http.Handler {
 	mux.HandleFunc("/snippet/view",app.snippetView)
 	mux.HandleFunc("/snippet/create",app.snippetCreate)
 	
-	return app.recoverPanic(app.logRequest(secureHeaders(mux))) // the requests now go through the middleware secureHeaders first
+	standard := alice.New(app.recoverPanic,app.logRequest,secureHeaders)
+
+	return standard.Then(mux) // Middleware chain using alice package
+
+	return app.recoverPanic(app.logRequest(secureHeaders(mux))) // Vanilla middleware chain
 }
