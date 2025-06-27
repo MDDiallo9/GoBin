75b62cbdded7895fc85465ffd5582a62bce2b850
Mohamed LN-WSL <114567817+MDDiallo9@users.noreply.github.com>
2025-06-27 11:12:10 +0200

    Added the validator to snippet creation handler

--- cmd/web/handlers.go
+++ cmd/web/handlers.go
@@ -2,27 +2,29 @@ package main
 
 import (
 	"fmt"
-	"strings"
-	"unicode/utf8"
+	/* "strings"
+	"unicode/utf8" */
+
 	//"html/template"
 	"errors"
 	"net/http"
 	"strconv"
 
 	"GoBin/internal/models"
+	"GoBin/internal/validator"
 
 	"github.com/julienschmidt/httprouter"
 )
 
 type snippetCreateForm struct {
-	Title string
-	Content string
-	Expires int
+	Title       string
+	Content     string
+	Expires     int
 	FieldErrors map[string]string
+	validator.Validator
 }
 
 func (app *application) home(w http.ResponseWriter, r *http.Request) {
-
 	snippets, err := app.snippets.Latest()
 	if err != nil {
 		app.serverError(w, err)
@@ -31,8 +33,7 @@ func (app *application) home(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Snippets = snippets
 
-	app.render(w,http.StatusOK,"home.tmpl",data)
-
+	app.render(w, http.StatusOK, "home.tmpl", data)
 }
 
 func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
@@ -54,66 +55,54 @@ func (app *application) snippetView(w http.ResponseWriter, r *http.Request) {
 	}
 	if r.URL.Query().Get("dl") != "" {
 		w.Header().Set("Content-Disposition", fmt.Sprintf("attachment; filename=snippet-%v.txt", snippet.Title))
-        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
-        w.Write([]byte(snippet.Content))
-        return
+		w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+		w.Write([]byte(snippet.Content))
+		return
 	}
-	
-	
 
 	data := app.newTemplateData(r)
 	data.Snippet = snippet
 
-	app.render(w,http.StatusOK,"view.tmpl",data)
-	
+	app.render(w, http.StatusOK, "view.tmpl", data)
 }
 
-func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request){
+func (app *application) snippetCreate(w http.ResponseWriter, r *http.Request) {
 	data := app.newTemplateData(r)
 	data.Form = snippetCreateForm{
 		Expires: 365,
 	}
-	app.render(w,http.StatusOK,"create.tmpl",data)
+	app.render(w, http.StatusOK, "create.tmpl", data)
 }
 
 func (app *application) snippetCreatePost(w http.ResponseWriter, r *http.Request) {
 	err := r.ParseForm()
 	if err != nil {
-		app.clientError(w,http.StatusBadRequest)
+		app.clientError(w, http.StatusBadRequest)
 	}
 
-	expires,err := strconv.Atoi(r.PostForm.Get("expires"))
+	expires, err := strconv.Atoi(r.PostForm.Get("expires"))
+
 	form := snippetCreateForm{
-		Title: r.PostForm.Get("title"),
-		Content:r.PostForm.Get("content"),
-		Expires: expires,
-		FieldErrors: make(map[string]string),
+		Title:       r.PostForm.Get("title"),
+		Content:     r.PostForm.Get("content"),
+		Expires:     expires,
 	}
 
 	if err != nil {
-		app.clientError(w,http.StatusBadRequest)
+		app.clientError(w, http.StatusBadRequest)
 	}
 
 	// Checking inputs
+	form.CheckField(validator.NotBlank(form.Title), "title", "This field cannot be blank")
+	form.CheckField(validator.MaxChars(form.Title, 100), "title", "This field cannot be more than 100 characters long")
+	form.CheckField(validator.NotBlank(form.Content), "content", "This field cannot be blank")
+	form.CheckField(validator.PermittedInt(form.Expires, 1, 7, 365), "expires", "This field must equal 1, 7 or 365")
 
-	if strings.TrimSpace(form.Title) == "" {
-		form.FieldErrors["title"] = "This field cannot be blank"
-	} else if utf8.RuneCountInString(form.Title) > 100 { // better than len(title) because len only counts bytes , this one counts runes
-		form.FieldErrors["title"] = "This field cannot be longer than a 100 characters long"
-	}
-
-	if strings.TrimSpace(form.Content) == "" {
-		form.FieldErrors["content"] = "This field cannot be blank"
-	}
-
-	if expires != 1 && expires != 7 && expires != 365 {
-		form.FieldErrors["expires"] = "This field must be either 1,7 or 365"
-	}
-
-	if len(form.FieldErrors) > 0 {
+	if !form.Valid() {
 		data := app.newTemplateData(r)
+		form.FieldErrors = form.Validator.FieldErrors
 		data.Form = form
-		app.render(w,http.StatusOK,"create.tmpl",data)
+		app.render(w,http.StatusUnprocessableEntity,"create.tmpl",data)
 		return
 	}
 
@@ -134,7 +123,7 @@ func (app *application) snippetDelete(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	del,err := app.snippets.Delete(id)
+	del, err := app.snippets.Delete(id)
 	if err != nil {
 		if errors.Is(err, models.ErrNoRecords) {
 			app.notFound(w)
@@ -143,6 +132,6 @@ func (app *application) snippetDelete(w http.ResponseWriter, r *http.Request) {
 		}
 		return
 	}
-	fmt.Printf("Deleted snippet #%v\n",del)
-	http.Redirect(w,r,"/",http.StatusSeeOther)
+	fmt.Printf("Deleted snippet #%v\n", del)
+	http.Redirect(w, r, "/", http.StatusSeeOther)
 }
